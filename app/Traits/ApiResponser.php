b7eddb0dafc108f65dc930672c53732c67c43180
JuanDMeGon <juandmegon@gmail.com>
2018-10-19 13:51:00 -0500

    Unifying Lumen responses for the API Gateway

---
+++ app/Traits/ApiResponser.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\Response;
+
+trait ApiResponser
+{
+    /**
+     * Build success response
+     * @param  string|array $data
+     * @param  int $code
+     * @return Illuminate\Http\Response
+     */
+    public function successResponse($data, $code = Response::HTTP_OK)
+    {
+        return response($data, $code)->header('Content-Type', 'application/json');
+    }
+
+    /**
+     * Build error responses
+     * @param  string|array $message
+     * @param  int $code
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function errorResponse($message, $code)
+    {
+        return response()->json(['error' => $message, 'code' => $code], $code);
+    }
+
+    /**
+     * Build error responses
+     * @param  string|array $message
+     * @param  int $code
+     * @return Illuminate\Http\Response
+     */
+    public function errorMessage($message, $code)
+    {
+        return response($message, $code)->header('Content-Type', 'application/json');
+    }
+}
