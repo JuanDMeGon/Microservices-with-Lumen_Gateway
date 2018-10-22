147cd4b41807aba51e89a7b69aa7994b823d9d79
JuanDMeGon <juandmegon@gmail.com>
2018-10-22 14:41:31 -0500

    Protecting the Gateway routes with Lumen Passport

--- routes/web.php
+++ routes/web.php
@@ -11,22 +11,24 @@
 |
 */
 
-/**
- * Routes for authors
- */
-$router->get('/authors', 'AuthorController@index');
-$router->post('/authors', 'AuthorController@store');
-$router->get('/authors/{author}', 'AuthorController@show');
-$router->put('/authors/{author}', 'AuthorController@update');
-$router->patch('/authors/{author}', 'AuthorController@update');
-$router->delete('/authors/{author}', 'AuthorController@destroy');
+$router->group(['middleware' => 'client.credentials'], function () use ($router) {
+    /**
+     * Routes for authors
+     */
+    $router->get('/authors', 'AuthorController@index');
+    $router->post('/authors', 'AuthorController@store');
+    $router->get('/authors/{author}', 'AuthorController@show');
+    $router->put('/authors/{author}', 'AuthorController@update');
+    $router->patch('/authors/{author}', 'AuthorController@update');
+    $router->delete('/authors/{author}', 'AuthorController@destroy');
 
-/**
- * Routes for books
- */
-$router->get('/books', 'BookController@index');
-$router->post('/books', 'BookController@store');
-$router->get('/books/{book}', 'BookController@show');
-$router->put('/books/{book}', 'BookController@update');
-$router->patch('/books/{book}', 'BookController@update');
-$router->delete('/books/{book}', 'BookController@destroy');
+    /**
+     * Routes for books
+     */
+    $router->get('/books', 'BookController@index');
+    $router->post('/books', 'BookController@store');
+    $router->get('/books/{book}', 'BookController@show');
+    $router->put('/books/{book}', 'BookController@update');
+    $router->patch('/books/{book}', 'BookController@update');
+    $router->delete('/books/{book}', 'BookController@destroy');
+});
